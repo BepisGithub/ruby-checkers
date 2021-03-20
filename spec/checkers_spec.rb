580101988c4c0acacde1f560a815fde77ad474d4
bepis <luqdek@gmail.com>
2021-03-20 21:54:35 +0000

    added a test to make sure the board class creates a graph which has the first node as the 1, 8 coordinate

--- spec/checkers_spec.rb
+++ spec/checkers_spec.rb
@@ -6,4 +6,8 @@ describe Board do
     board = Board.new
     expect(board).to be_truthy
   end
+  it 'starts at coordinate [1, 8]' do
+    board = Board.new
+    expect(board.graph.head.data.coordinate).to eql([1, 8])
+  end
 end
