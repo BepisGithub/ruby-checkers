abf457b86789418cacb3e89683c1476e228a5fbb
bepis <luqdek@gmail.com>
2021-03-20 21:26:19 +0000

    added a test for the board class to be able to call the functions it needs to

--- spec/checkers_spec.rb
+++ spec/checkers_spec.rb
@@ -1,2 +1,9 @@
 require './spec/spec_helper'
 require './scripts'
+
+describe Board do
+  it 'doesnt throw a syntax error when intializing the board' do
+    board = Board.new
+    expect(board).to be_truthy
+  end
+end
