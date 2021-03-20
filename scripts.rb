d350d0bc8b6c2a2b6f89dcd354ab3a8a9ac66376
bepis <luqdek@gmail.com>
2021-03-20 20:59:26 +0000

    changed instance variable name next to next_node

--- scripts.rb
+++ scripts.rb
@@ -1,9 +1,9 @@
 class LinkedNode
-  attr_accessor :data ,:next
+  attr_accessor :data, :next_node
 
   def initialize(data, next_node = nil)
     @data = data
-    @next = next_node
+    @next_node = next_node
   end
 
 end
